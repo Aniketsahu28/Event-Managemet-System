b825c99938808edb8953a95da004db0502259a76
Aniket <aniketsahu1711@gmail.com>
2025-01-24 21:02:12 +0530

    [Refacor]:Changed loading animation

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
-import 'ldrs/hourglass'
 
 const Loader = ({ message }) => {
     return (
         <div
             className={`fixed h-screen w-screen flex flex-col gap-10 text-white items-center justify-center bg-black/80 z-30 top-0 left-0`}
         >
-            <l-hourglass
-                size="50"
-                bg-opacity="0.3"
-                speed="1.8"
-                color="white"
-            />
+            <div class="loader"></div>
             <p className='sm:text-xl'>{message}</p>
         </div>
     )
